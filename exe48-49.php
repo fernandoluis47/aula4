36302a46426a9107d1577523f7351e1579823375
fernando <lfernando358@gmail.com>
2021-12-01 17:29:46 -0300

    first commit

---
+++ exe48-49.php
@@ -0,0 +1,12 @@
+<?php
+
+//exercicio 48 e 49
+$resp = "S";
+while($resp =="S"){
+    $nota1 = 10;
+    $nota2 = 5;
+    $media = ($nota1 + $nota2)/2;
+    echo "{$media} - ";
+    //deseja continuar
+    $resp = "n";
+}
---
+++ exe7.php
@@ -0,0 +1,8 @@
+<?php
+
+$idade = 45;
+$meses = 12* $idade;
+echo "Você viveu {$meses} meses<br>";
+echo "<hr>";
+$dias = 360 * $idade;
+echo "Você viveu {$dias} dias<br>";
